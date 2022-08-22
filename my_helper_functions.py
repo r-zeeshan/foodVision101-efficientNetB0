857ea064ad18bce3d6e462de2949527e9e7c34f1
r-zeeshan <111675443+r-zeeshan@users.noreply.github.com>
2022-08-22 11:22:49 +0500

    Update my_helper_functions.py

--- my_helper_functions.py
+++ my_helper_functions.py
@@ -160,7 +160,7 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
         fig.savefig("confusion_matrix.png")
 
 import tensorflow as tf
-def load_and_prep_image(filename, img_shape=224, scale=True):
+def load_and_prep_image(filename, img_shape=224, scale=False):
     """
     Reads in an image from filename, turns it into a tensor and reshapes into
     (224, 224, 3).
@@ -182,13 +182,13 @@ def load_and_prep_image(filename, img_shape=224, scale=True):
     else:
         return img
 
-def pred_and_plot(model, filename, class_names, scale=True):
+def pred_and_plot(model, filename, class_names):
     """
     Imports an image located at filename, makes a prediction on it with
     a trained model and plots the image with the predicted class as the title.
     """
     # Import the target image and preprocess it
-    img = load_and_prep_image(filename, scale=scale)
+    img = load_and_prep_image(filename, scale)
 
     # Make a prediction
     pred = model.predict(tf.expand_dims(img, axis=0))
@@ -200,7 +200,7 @@ def pred_and_plot(model, filename, class_names, scale=True):
         pred_class = class_names[int(tf.round(pred)[0][0])] # if only one output, round
 
     # Plot the image and predicted class
-    plt.imshow(img)
+    plt.imshow(img/255.)
     plt.title(f"Prediction: {pred_class}")
     plt.axis(False);
 
