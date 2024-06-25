45228f7573fa21c7f1c5e25c015794f9271d099f
Zeeshan <fiverr.zeeshanrajpoot@gmail.com>
2024-06-25 14:47:50 +0000

    add files for loading and preprocessing data

---
+++ dataset.py
@@ -0,0 +1,22 @@
+import tensorflow_datasets as tfds
+
+
+
+def get_dataset(name, split):
+    """
+    Loads and returns the specified dataset.
+
+    Parameters:
+    - name (str): The name of the dataset to load.
+    - split (str): The split of the dataset to load (e.g., 'train', 'test').
+
+    Returns:
+    - tuple: A tuple containing the train data, test data, and dataset information.
+    """
+    (train_data, test_data), ds_info = tfds.load(name=name,
+                                                 split=split,
+                                                 shuffle_files=False,
+                                                 as_supervised=True,
+                                                 with_info=True)
+
+    return (train_data, test_data), ds_info
---
+++ preprocess.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+
+
+def resize_image(image, label, image_shape=224):
+    """
+    Resize the input image to the specified shape.
+
+    Args:
+        image (tf.Tensor): The input image tensor.
+        label (tf.Tensor): The label tensor.
+        image_shape (int): The desired shape of the image (default: 224).
+
+    Returns:
+        tf.Tensor: The resized image tensor.
+        tf.Tensor: The label tensor.
+    """
+    image = tf.image.resize(image, [image_shape, image_shape])
+    return tf.case(image, tf.float32), label
+
+
+
+def batch_data(train_data, test_data):
+    """
+    Preprocesses and batches the train and test data.
+
+    Args:
+        train_data (tf.data.Dataset): The training data.
+        test_data (tf.data.Dataset): The test data.
+
+    Returns:
+        tuple: A tuple containing the preprocessed and batched train data and test data.
+    """
+    train_data = train_data.map(map_func=resize_image,
+                                num_parallel_calls=tf.data.AUTOTUNE
+                                ).shuffle(1000).batch(32).prefetch(tf.data.AUTOTUNE)
+
+    test_data = test_data.map(map_func=resize_image,
+                                num_parallel_calls=tf.data.AUTOTUNE
+                                ).shuffle(1000).batch(32).prefetch(tf.data.AUTOTUNE)
+
+    return train_data, test_data
+
+    
