0bed7dbd0bd0cb480b512aa113f61cb2fa3e1a68
Zeeshan <fiverr.zeeshanrajpoot@gmail.com>
2024-06-26 08:51:21 +0000

    completed train.py and made various design changes in code

--- train.py
+++ train.py
@@ -2,24 +2,47 @@ import tensorflow as tf
 from dataset import get_dataset
 from preprocess import batch_data
 from architecture import create_model
+from callbacks import get_callbacks
+from config import *
+import pickle
+import os
 
 
 ### Setting a mixed precision global policy
-tf.keras.mixed_precision.set_global_policy(policy="mixed_floats")
+tf.keras.mixed_precision.set_global_policy(policy="mixed_float16")
 
 
-### Defining Some global variables
-BASE = tf.keras.applications.EfficientNetB3(include_top=False)
-SHAPE = (224, 224, 3)
-POOLING = tf.keras.Layers.GlobalAveragePooling2D(name="GlobalAveragePooling2D")
-LOSS = 'sparse_categorical_crossentropy'
+### Loading and preprocessing the Dataset
+(train_data, test_data), ds_info = get_dataset(name=DATASET_NAME, split=DATASET_SPLIT)
+
+train_data, test_data = batch_data(train_data=train_data, test_data=test_data)
+
+class_names = ds_info.features['label'].names
 
 
+### Creating the model
+model = create_model(base=BASE,shape=SHAPE, pooling=POOLING, activation=ACTIVATION, class_names=class_names, loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
 
 
+### Setting up callbacks
+callbacks = get_callbacks()
 
 
+### Training the Model
+history = model.fit(train_data,
+                    epochs=50,
+                    steps_per_epoch=len(train_data),
+                    validation_data=test_data,
+                    validation_steps=int(0.1 * len(test_data)),
+                    callbacks=callbacks)
 
 
+os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+os.makedirs(os.path.dirname(HISTORY_PATH), exist_ok=True)
 
 
+### Saving the Model and the history
+model.save(MODEL_PATH)
+
+with open(HISTORY_PATH, 'wb') as file:
+    pickle.dump(history.history, file)
