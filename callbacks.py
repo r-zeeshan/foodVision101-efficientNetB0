9a32c7605e57d7ec33131edc99b819cdbb26a0c3
Zeeshan <fiverr.zeeshanrajpoot@gmail.com>
2024-06-26 07:27:49 +0000

    create callbacks.py containing the callbacks, used in model training

---
+++ callbacks.py
@@ -0,0 +1,80 @@
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, TensorBoard
+import datetime
+
+class CallBacks:
+
+    @staticmethod
+    def model_checkpoint(path='modelCheckPoints/checkpoint.ckpt'):
+        """
+        Create a ModelCheckpoint callback.
+
+        Args:
+            path (str): The path to save the model checkpoints.
+
+        Returns:
+            ModelCheckpoint: The ModelCheckpoint callback object.
+
+        """
+        return ModelCheckpoint(path,
+                               monitor='val_accuracy',
+                               save_best_only=True,
+                               save_weights_only=True,
+                               verbose=0,
+                               save_freq='epoch')
+
+
+    @staticmethod    
+    def early_stopping():
+        """
+        Create an EarlyStopping callback.
+
+        Returns:
+            EarlyStopping: The EarlyStopping callback object.
+        """
+        return EarlyStopping(monitor='val_loss', patience=3)
+    
+
+    @staticmethod
+    def reduce_lr():
+        """
+        Create a ReduceLROnPlateau callback.
+
+        Returns:
+            ReduceLROnPlateau: The ReduceLROnPlateau callback object.
+        """
+        return ReduceLROnPlateau(monitor='val_loss',
+                                 factor=0.2,
+                                 patience=2,
+                                 verbose=1,
+                                 min_lr=1e-7)
+
+
+    
+    @staticmethod
+    def tensorboard(dir_name='food_vision', exp_name='efficentnetb3'):
+        """
+        Creates a TensorBoard callback instand to store log files.
+        Stores log files with the filepath:
+            "dir_name/experiment_name/current_datetime/"
+        Args:
+            dir_name: target directory to store TensorBoard log files
+            experiment_name: name of experiment directory (e.g. efficientnet_model_1)
+        """
+
+        log_dir = dir_name + '/' + exp_name + '/' + datetime.dateime.now().strftime("%Y%m%d-%H%M%S")        
+        return TensorBoard(log_dir=log_dir)
+    
+
+    @staticmethod
+    def get_callbacks():
+        return [CallBacks.early_stopping(),
+                CallBacks.reduce_lr(),
+                CallBacks.model_checkpoint(),
+                CallBacks.tensorboard()]
+
+
+
+    
+    
+
+
